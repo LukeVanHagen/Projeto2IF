4ec67a6150d5705eafd027f220db1961d2673239
Pancosta <dz6.gustav@gmail.com>
2023-05-07 17:11:27 -0300

    correção import 2x do controler em web.php

--- PsicoMais/routes/web.php
+++ PsicoMais/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ConsultController1;
 use App\Http\Controllers\ConsultsController1;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
