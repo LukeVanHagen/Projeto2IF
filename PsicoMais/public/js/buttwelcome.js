5136ad61cd68e772fa691f2b3ce42663926a1383
Cecilia <ceciliacarolinace2@gmail.com>
2023-05-12 16:58:15 -0300

    Estilizando a pagina de boas vindas para usuários

---
+++ PsicoMais/public/js/buttwelcome.js
@@ -0,0 +1,12 @@
+//Codigo script show an hidden do arquivo welcome.blade.php
+
+function toggleHistoria() {
+    var textoHistoria = document.getElementById("textoHistoria");
+    if (textoHistoria.style.display === "none") {
+      textoHistoria.style.display = "block";
+      document.getElementById("btnHistoria").innerHTML = " História <";
+    } else {
+      textoHistoria.style.display = "none";
+      document.getElementById("btnHistoria").innerHTML = "História >";
+    }
+  }
--- PsicoMais/resources/views/welcome.blade.php
+++ PsicoMais/resources/views/welcome.blade.php
@@ -4,98 +4,78 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PSICOMAIS</title>
         
         <!-- Fonts -->
+        <link href="{{ asset('css/stylewelcome.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <!-- Styles -->
         <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-black-500{--text-opacity:1;color:#000000;color:rgba(0,0,0,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-right{text-align:right}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-black-500{--text-opacity:1;color:#000000;color:rgba(0,0,0,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-right{text-align:right}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
         </style>
 
         <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-               
-            }
-            img {
-                max-width: 400%;
-                width: 600px;
-                float: left;
-            }
-
-            #btnHistoria{
-                background-color: #94baeb; /* altera a cor do botão */
-                color: #fff; /* altera a cor do texto do botão */
-                border-radius: 20px; /* faz com que as pontas do botão fiquem arredondadas */
-                padding: 10px 20px; /* aumenta o tamanho do botão */
-                font-size: 16px; /* altera o tamanho do texto do botão */
-                border: none;  /* remove a borda do botão */
-                cursor: pointer; /* faz com que o cursor mude quando o mouse passa sobre o botão */
-
-            }
-            .titulo {
-               font-size: 3rem; /* tamanho grande */
-               position: absolute; /* posicionamento absoluto */
-               top: 0; /* distância do topo */
-               center: 0; /* distância da esquerda */
-            background-color:trasparent;
-               color:#3b82f6;
-            }
-              
+           body {
+              font-family: 'Nunito', sans-serif;
+             }
               </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+    <!--Corpo da página -->
+    <body class="antialiased ">
+        <div class="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+          <!--Navbar com login e registro -->
+        <div class="w3-top ">
+                <div class="w3-bar w3-white  w3-padding w3-card">
+
+                  <a href="#" class="w3-bar-item ">
+                  <b class="text-lg font-semibold">Psico+</b>
+                  </a>
+                  @if (Route::has('login'))
+                  <div class="w3-right w3-hide-small">
+                     @auth
+                    <a href="{{ url('/dashboard') }}" class="w3-bar-item  text-sm">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
+                    <button class="btt-1">
+                    <a href="{{ route('login') }}" class=" w3-bar-item font-semibold">Login</a>
+                  </button> 
+                   @if (Route::has('register'))
+                   <button class="btt-2">
+                    <a href="{{ route('register') }}" class="w3-bar-item  font-semibold ">Registrar</a>
+                 </button>  
+                   @endif
                     @endauth
-                </div>
-            @endif
-            
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                   <h1 class="titulo">Bem Vindos ao PsicoMais</h1>
-                </div>
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6">
-                     <div>
-                        <img src="{{ asset('images/psicomais.jpeg') }}" alt="Minha imagem">  
-                    </div>     
-
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                <div class="text-center text-sm text-gray-500 sm:text-left">
-                        
-            <button id="btnHistoria" onclick="toggleHistoria()">História</button>
-              <div id="textoHistoria" style="display:none">Era uma vez um lindo passaro azul,que morava numa linda bola azul,que flutuava pelo espaco,com muitas florestas e muitos bixos</div>
-
-        <script>
-    function toggleHistoria() {
-    var textoHistoria = document.getElementById("textoHistoria");
-    if (textoHistoria.style.display === "none") {
-      textoHistoria.style.display = "block";
-      document.getElementById("btnHistoria").innerHTML = " História";
-    } else {
-      textoHistoria.style.display = "none";
-      document.getElementById("btnHistoria").innerHTML = "História";
-    }
-  }
-        </script>
-
                   </div>
+                    @endif
                 </div>
-            </div> 
-        </div>
+
+            </div>
+          <!--Fim da navbar-->
+           
+<!--Div com o contéudo-->
+           <div class="relative-div">
+              <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden  sm:rounded-lg">
+                  <div class="grid ">
+                    <div>
+                        <img src="{{ asset('images/psicomais.jpeg') }}" alt="Minha imagem">  
+                    </div> 
+                 </div>
+                 <!--Div com o titulo bem vindos-->
+                    <div class="absolute-1-div">   
+                      <h1 class="titulo text-1">Bem Vindos ao Psico+</h1>
+                    </div>
+                 <!--Fim da div com o titulo-->
+              <!--Div com o botão leia mais ou história-->
+                <div class="absolute-2-div">   
+                   <button id="btnHistoria" class="font-semibold " onclick="toggleHistoria()">História ></button>
+                </div>
+              <!--Fim da div do botão hitoria-->
+<!--Div com a historia ou informações-->
+                  <div id="textoHistoria" style="display:none" class="absolute-0-div font-semibold text-gray-200" >Era uma vez um lindo passaro azul,que morava numa linda bola azul,que flutuava pelo espaco,com muitas florestas e muitos bixos</div>
+                </div>
+              </div>
+            </div>
+        <!--Fim da div com contéudo-->
+<script src="{{ asset('js/buttwelcome.js') }}"></script>
     </body>
 </html>
