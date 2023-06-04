6397c5756ca81877160d1a87ba539b25f6ea13ae
Pancosta <dz6.gustav@gmail.com>
2023-06-04 13:09:14 -0300

    o mesmo profissional não consegue cadastrar consultas em horarios que se chocam

--- PsicoMais/app/Http/Controllers/ConsultController.php
+++ PsicoMais/app/Http/Controllers/ConsultController.php
@@ -23,6 +23,8 @@ class ConsultController extends Controller
         $consults = collect();
         $current_date = $validatedData['date'];
     
+        $dateModified = false; // Variável para rastrear se a data já foi modificada
+    
         for ($i = 0; $i < $validatedData['period']; $i++) {
             $consult = new Consult;
             $consult->profissional_id = Auth::id();
@@ -32,11 +34,57 @@ class ConsultController extends Controller
             $consult->time = date('H:i', $start_time + ($i * 3600));
             $consult->end_time = date('H:i', $start_time + (($i + 1) * 3600));
     
-            // Adicionar 1 dia se o horário final for 00:00
-            if ($consult->end_time === "00:00") {
+            if ($consult->end_time >= "00:00" && !$dateModified) {
                 $current_date = date('Y-m-d', strtotime($current_date . ' +1 day'));
+                $dateModified = true;
+            } elseif ($consult->end_time <= "00:00") {
+                $dateModified = false; // Reinicia a variável para a próxima ocorrência de ultrapassar a meia-noite
             }
-            
+    
+            // Verifica se já existe uma consulta do mesmo profissional no mesmo horário
+            $existingConsult = Consult::where('profissional_id', $consult->profissional_id)
+                ->where('date', $consult->date)
+                ->where(function ($query) use ($consult) {
+                    $query->where(function ($q) use ($consult) {
+                        $q->where('time', '>=', $consult->time)
+                            ->where('time', '<', $consult->end_time);
+                    })
+                    ->orWhere(function ($q) use ($consult) {
+                        $q->where('time', '<=', $consult->time)
+                            ->where('end_time', '>', $consult->time);
+                    })
+                    ->orWhere(function ($q) use ($consult) {
+                        $q->where('time', '>=', $consult->time)
+                            ->where('end_time', '<=', $consult->end_time);
+                    });
+                })
+                ->first();
+    
+            if ($existingConsult) {
+                return redirect()->route('consult.create')->with('msg', 'Já existe uma consulta disponibilizada nesse horário.');
+            }
+    
+            // Verifica se já existe uma consulta do mesmo profissional durante o intervalo de horário
+            $overlappingConsult = Consult::where('profissional_id', $consult->profissional_id)
+                ->where('date', $consult->date)
+                ->where(function ($query) use ($consult) {
+                    $query->where('time', '<', $consult->time)
+                        ->where('end_time', '>', $consult->time);
+                })
+                ->orWhere(function ($query) use ($consult) {
+                    $query->where('time', '>=', $consult->time)
+                        ->where('end_time', '<=', $consult->end_time);
+                })
+                ->orWhere(function ($query) use ($consult) {
+                    $query->where('time', '<', $consult->end_time)
+                        ->where('end_time', '>', $consult->end_time);
+                })
+                ->first();
+    
+            if ($overlappingConsult) {
+                return redirect()->route('consult.create')->with('msg', 'Já existe uma consulta durante o intervalo desse horário.');
+            }
+    
             $consults->push($consult);
         }
     
