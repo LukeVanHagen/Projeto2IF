6029edb8cc34fb3874ef1bd1b51bacbd69705fbc
LukeVanHagen <flgs@discente.ifpe.edu.br>
2023-06-29 05:57:30 -0300

    Adicionando opção de cadastrar-se na tela de login

--- PsicoMais/resources/views/auth/login.blade.php
+++ PsicoMais/resources/views/auth/login.blade.php
@@ -33,11 +33,8 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Esqueceu sua Senha?') }}
-                </a>
-            @endif
+            
+           <x-primary-button><a href="{{ route('register') }}" class="w3-bar-item font-semibold ">Registrar-se</a></x-primary-button>
 
             <x-primary-button class="ml-3">
                 {{ __('Entrar') }}
