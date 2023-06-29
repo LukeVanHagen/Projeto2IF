96f2f65dcd57e1d14a1667225f634d813a1db9aa
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-06-29 07:39:49 -0300

    Alterando somente o nome do botão para leia mais buttwelcome.js

--- PsicoMais/public/js/buttwelcome.js
+++ PsicoMais/public/js/buttwelcome.js
@@ -4,9 +4,9 @@ function toggleHistoria() {
     var textoHistoria = document.getElementById("textoHistoria");
     if (textoHistoria.style.display === "none") {
       textoHistoria.style.display = "block";
-      document.getElementById("btnHistoria").innerHTML = " História <";
+      document.getElementById("btnHistoria").innerHTML = " Leia Mais <";
     } else {
       textoHistoria.style.display = "none";
-      document.getElementById("btnHistoria").innerHTML = "História >";
+      document.getElementById("btnHistoria").innerHTML = "Leia Mais >";
     }
   }
